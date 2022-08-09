b2e54e02944110232d0c54cfb85d285b1d9c04ec
Your Name <miriti9595@gmail.com>
2022-08-09 11:05:21 -0700

    main.h

--- test/main.c
+++ test/main.c
@@ -6,7 +6,6 @@
  * main - Entry point
  * Return: Always 0
  */
-int _putchar(char c);
 int main(void)
 {
 int len;
@@ -38,6 +37,5 @@ len2 = printf("Percent:[%%]\n");
 _printf("Len:[%d]\n", len);
 printf("Len:[%d]\n", len2);
 _printf("Unknown:[%r]\n");
-printf("Unknown:[%r]\n");
 return (0);
 }
